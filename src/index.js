3376d134e2173dc559b46a6609a274490eb96935
Raman Karseka <ier@live.ru>
2021-03-16 01:12:32 +0300

    feat: solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+	if (n>0)
+	return Number(String(n).split("").reverse().join(""))
+else return Number(String(n).substr(1).split("").reverse().join(""))  
 }
